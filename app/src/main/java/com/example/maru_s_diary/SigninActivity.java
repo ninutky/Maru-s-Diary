74b7800932e797ec0b380f1a26b9b13d8e4117df
Yu Jung Eun <0hh.0553@gmail.com>
2023-03-06 14:00:36 +0900

    로그인 화면의 tvbtn_signup -> 회원가입 페이지 전환

--- app/src/main/java/com/example/maru_s_diary/SigninActivity.java
+++ app/src/main/java/com/example/maru_s_diary/SigninActivity.java
@@ -2,13 +2,27 @@ package com.example.maru_s_diary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SigninActivity extends AppCompatActivity {
+    TextView tvbtn_signup, tvbtn_pwfind;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+        tvbtn_signup = findViewById(R.id.tvbtn_signup);
+        tvbtn_pwfind = findViewById(R.id.tvbtn_pwfind);
+
+        tvbtn_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
