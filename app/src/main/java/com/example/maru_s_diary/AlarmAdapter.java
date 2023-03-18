116ad18821972a7cf36d806842e0659a62ce1f50
cuteNK <zzum0701@naver.com>
2023-03-19 07:52:05 +0900

    알림창과 알림버튼 연결

---
+++ app/src/main/java/com/example/maru_s_diary/AlarmAdapter.java
@@ -0,0 +1,5 @@
+package com.example.maru_s_diary;
+
+public class AlarmAdapter {
+
+}
--- app/src/main/java/com/example/maru_s_diary/MainActivity.java
+++ app/src/main/java/com/example/maru_s_diary/MainActivity.java
@@ -1,28 +1,39 @@
 package com.example.maru_s_diary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
-    private FragmentManager fragmentManager;
-    private PwChangeFragment fragmentA;
-    private FragmentTransaction transaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        fragmentManager = getSupportFragmentManager();
-
-        fragmentA = new PwChangeFragment();
-
-//        transaction = fragmentManager.beginTransaction();
-//        transaction.replace(R.id.frameLayout, fragmentB).commitAllowingStateLoss();
-
         setContentView(R.layout.activity_main);
+
+        // 알림 버튼을 눌렀을 때 실행
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.action_alarm) { // 알람 버튼 클릭 이벤트 처리
+                    // 프래그먼트 이동
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.container, new AlarmFragment());
+                    fragmentTransaction.addToBackStack(null);
+                    fragmentTransaction.commit();
+                    return true;
+                }
+                return false;
+            }
+        });
+
     }
+
 }
