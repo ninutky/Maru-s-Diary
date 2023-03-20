3ee6b2cfff101d3c5c425d184fc684896282ae15
cuteNK <zzum0701@naver.com>
2023-03-20 19:59:08 +0900

    알림 설정 페이지 진행중

--- app/src/main/java/com/example/maru_s_diary/AlarmSettingFragment.java
+++ app/src/main/java/com/example/maru_s_diary/AlarmSettingFragment.java
@@ -3,27 +3,26 @@ package com.example.maru_s_diary;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class AlarmFragment extends Fragment {
-
-    private AlarmAdapter rvAdapter = new AlarmAdapter();
+public class AlarmSettingFragment extends Fragment {
+    public AlarmSettingFragment() {
+        // Required empty public constructor
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
 
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_alarm, container, false);
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_alarm_setting, container, false);
     }
-
 }
