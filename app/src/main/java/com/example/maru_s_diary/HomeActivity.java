f427389b84f0b26ac811474ce4baa36a84575c2d
Yu Jung Eun <0hh.0553@gmail.com>
2023-03-23 09:42:16 +0900

    홈화면 recyclerview, floatingbutton

--- app/src/main/java/com/example/maru_s_diary/HomeActivity.java
+++ app/src/main/java/com/example/maru_s_diary/HomeActivity.java
@@ -1,8 +1,12 @@
 package com.example.maru_s_diary;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -23,5 +27,24 @@ public class HomeActivity extends AppCompatActivity {
 
 
         recyclerView.setAdapter(adapter);
+
+        // 알림 버튼을 눌렀을 때 실행
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.action_alarm) { // 알람 버튼 클릭 이벤트 처리
+                    // 프래그먼트 이동
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.container, new AlarmFragment());
+                    fragmentTransaction.addToBackStack(null);
+                    fragmentTransaction.commit();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
