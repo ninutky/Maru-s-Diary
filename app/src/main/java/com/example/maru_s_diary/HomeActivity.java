38f53e434da6d0b68e24dcad3ae5364f7d488f43
Yu Jung Eun <0hh.0553@gmail.com>
2023-03-25 18:16:20 +0900

    home activity -> fragment

--- app/src/main/java/com/example/maru_s_diary/HomeActivity.java
+++ app/src/main/java/com/example/maru_s_diary/HomeActivity.java
@@ -1,30 +1,41 @@
 package com.example.maru_s_diary;
 
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends Fragment {
     RecyclerView recyclerView;
     RecyclerView.Adapter adapter;
 
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.fragment_home);
+//
+//        LinearLayoutManager linearLayoutManager
+//                = new LinearLayoutManager(this);
+//
+//        recyclerView = findViewById(R.id.recyclerView);
+//        recyclerView.setLayoutManager(linearLayoutManager);
+//
+//
+//    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        recyclerView = v.findViewById(R.id.recyclerView);
         LinearLayoutManager linearLayoutManager
-                = new LinearLayoutManager(this);
-
-        recyclerView = findViewById(R.id.recyclerView);
+                = new LinearLayoutManager(getContext());
         recyclerView.setLayoutManager(linearLayoutManager);
-
-
+        return v;
     }
 }
