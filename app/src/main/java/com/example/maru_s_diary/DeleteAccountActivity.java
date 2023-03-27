266387ab838dcb98030dd41030a0372f82eb1f2e
Yu Jung Eun <0hh.0553@gmail.com>
2023-03-27 15:11:01 +0900

    DeleteAccountActivity 하는중

---
+++ app/src/main/java/com/example/maru_s_diary/DeleteAccountActivity.java
@@ -0,0 +1,14 @@
+package com.example.maru_s_diary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeleteAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_account);
+    }
+}
