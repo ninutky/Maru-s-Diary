6e1cf3a08c29a6d51fc78bb64e88f6ed96b8f2b9
Yu Jung Eun <0hh.0553@gmail.com>
2023-04-03 15:25:33 +0900

    로그아웃 다이얼로그 제작중

--- app/src/main/java/com/example/maru_s_diary/SettingsFragment.java
+++ app/src/main/java/com/example/maru_s_diary/SettingsFragment.java
@@ -4,20 +4,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.fragment.app.Fragment;
 
 public class SettingsFragment extends Fragment {
+    LinearLayout prefer_logout_llbtn;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
 
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_settings, container, false);
+        prefer_logout_llbtn = v.findViewById(R.id.prefer_logout_llbtn);
+        prefer_logout_llbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dia
+            }
+        });
         return v;
     }
 }
