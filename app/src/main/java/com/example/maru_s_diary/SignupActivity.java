b7aef04c60db885439c7cacbe2cec3cf272950ca
JAYUN <s2115@e-mirim.hs.kr>
2023-04-08 15:30:46 +0900

    회원가입 레이아웃 표시

--- app/src/main/java/com/example/maru_s_diary/SignupActivity.java
+++ app/src/main/java/com/example/maru_s_diary/SignupActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,6 +20,13 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class SignupActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
 
+
+
+    }
 
 }
