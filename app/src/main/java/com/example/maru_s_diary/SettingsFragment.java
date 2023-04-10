86b89a409a38ff95bbad0634e3aeaa3f521a1dab
Yu Jung Eun <0hh.0553@gmail.com>
2023-04-10 11:11:15 +0900

    activity_delete_account.xml 끝

--- app/src/main/java/com/example/maru_s_diary/SettingsFragment.java
+++ app/src/main/java/com/example/maru_s_diary/SettingsFragment.java
@@ -1,15 +1,17 @@
 package com.example.maru_s_diary;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class SettingsFragment extends Fragment {
-    LinearLayout prefer_logout_llbtn;
+    TextView prefer_delaccount_tvbtn;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,11 +21,12 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_settings, container, false);
-        prefer_logout_llbtn = v.findViewById(R.id.prefer_logout_llbtn);
-        prefer_logout_llbtn.setOnClickListener(new View.OnClickListener() {
+        prefer_delaccount_tvbtn = v.findViewById(R.id.prefer_delaccount_tvbtn);
+        prefer_delaccount_tvbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(getActivity(), DeleteAccountActivity.class);
+                startActivity(intent);
             }
         });
         return v;
