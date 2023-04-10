008b77e22415a13645cb1fdbd3bc81c8a83a4637
Ye-jin Lee <s2108@e-mirim.hs.kr>
2023-04-10 09:24:45 +0900

    일기 수정에 삭제 다이얼로그 완성

--- app/src/main/java/com/example/maru_s_diary/ClickDeleteDiary.java
+++ app/src/main/java/com/example/maru_s_diary/ClickDeleteDiary.java
@@ -1,40 +1,66 @@
 package com.example.maru_s_diary;
 
-
-import android.content.DialogInterface;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
-
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+
 public class ClickDeleteDiary extends AppCompatActivity {
 
+    Dialog dilaog01; // 커스텀 다이얼로그
+
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.click_delete_diary);
-    }
 
-    public void DialogClick(View view) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("삭제 하시겠습니까?");
-        builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
+        dilaog01 = new Dialog(ClickDeleteDiary.this);       // Dialog 초기화
+        dilaog01.requestWindowFeature(Window.FEATURE_NO_TITLE); // 타이틀 제거
+        dilaog01.setContentView(R.layout.dialog01);             // xml 레이아웃 파일과 연결
+
+        // 버튼: 커스텀 다이얼로그 띄우기
+        findViewById(R.id.showBtn).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Toast.makeText(getApplicationContext(), "삭제됨", Toast.LENGTH_LONG).show();
+            public void onClick(View view) {
+                showDialog01(); // 아래 showDialog01() 함수 호출
             }
         });
-        builder.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-
-            }
-        });
-
-        AlertDialog alertDialog = builder.create();
-        alertDialog.show();
     }
 
+    // dialog01을 디자인하는 함수
+    public void showDialog01(){
+        dilaog01.show(); // 다이얼로그 띄우기
+        dilaog01.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT)); // 투명 배경
+        /* 이 함수 안에 원하는 디자인과 기능을 구현하면 된다. */
+
+        // 위젯 연결 방식은 각자 취향대로~
+        // '아래 아니오 버튼'처럼 일반적인 방법대로 연결하면 재사용에 용이하고,
+        // '아래 네 버튼'처럼 바로 연결하면 일회성으로 사용하기 편함.
+        // *주의할 점: findViewById()를 쓸 때는 -> 앞에 반드시 다이얼로그 이름을 붙여야 한다.
+
+        // 아니오 버튼
+        Button noBtn = dilaog01.findViewById(R.id.noBtn);
+        noBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 원하는 기능 구현
+                dilaog01.dismiss(); // 다이얼로그 닫기
+            }
+        });
+        // 네 버튼
+        dilaog01.findViewById(R.id.yesBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 원하는 기능 구현
+                finish();           // 앱 종료
+            }
+        });
+    }
 }
