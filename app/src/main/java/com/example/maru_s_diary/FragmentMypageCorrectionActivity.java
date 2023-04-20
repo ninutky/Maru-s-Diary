aa0328f56b5d605dc50ffb0cd65ab4534c2f98b6
Yu Jung Eun <0hh.0553@gmail.com>
2023-04-21 08:40:12 +0900

    마이페이지 나의 일기 수정 페이지

---
+++ app/src/main/java/com/example/maru_s_diary/FragmentMypageCorrectionActivity.java
@@ -0,0 +1,14 @@
+package com.example.maru_s_diary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FragmentMypageCorrectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_mypage_correction);
+    }
+}
