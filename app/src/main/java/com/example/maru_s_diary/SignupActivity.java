e05b47643dee1fc9946fd991375946c4c9cf00b6
JAYUN <s2115@e-mirim.hs.kr>
2023-05-11 09:36:25 +0900

    firebase 회원가입

--- app/src/main/java/com/example/maru_s_diary/SignupActivity.java
+++ app/src/main/java/com/example/maru_s_diary/SignupActivity.java
@@ -1,14 +1,18 @@
 package com.example.maru_s_diary;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -19,14 +23,114 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.HashMap;
+
 public class SignupActivity extends AppCompatActivity {
+    private static final String TAG = "RegisterActivity";
+    EditText mEmailText, mPasswordText, mPasswordcheckText, mId;
+    Button mregisterBtn;
+    private FirebaseAuth firebaseAuth;
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+//        //액션 바 등록하기
+//        ActionBar actionBar = getSupportActionBar();
+//        actionBar.setTitle("Create Account");
+//
+//        actionBar.setDisplayHomeAsUpEnabled(true); //뒤로가기버튼
+//        actionBar.setDisplayShowHomeEnabled(true); //홈 아이콘
 
+        //파이어베이스 접근 설정
+        // user = firebaseAuth.getCurrentUser();
+        firebaseAuth =  FirebaseAuth.getInstance();
+        //firebaseDatabase = FirebaseDatabase.getInstance().getReference();
+
+        mEmailText = findViewById(R.id.emailEt);
+        mPasswordText = findViewById(R.id.et_signup_pwd);
+        mPasswordcheckText = findViewById(R.id.et_checksignup_pwd);
+        mregisterBtn = findViewById(R.id.btn_signup);
+        mId = findViewById(R.id.et_signup_id);
+
+        //파이어베이스 user 로 접글
+
+        //가입버튼 클릭리스너   -->  firebase에 데이터를 저장한다.
+        mregisterBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+                //가입 정보 가져오기
+                final String email = mEmailText.getText().toString().trim();
+                String pwd = mPasswordText.getText().toString().trim();
+                String pwdcheck = mPasswordcheckText.getText().toString().trim();
+
+
+                if(pwd.equals(pwdcheck)) {
+                    Log.d(TAG, "등록 버튼 " + email + " , " + pwd);
+                    final ProgressDialog mDialog = new ProgressDialog(SignupActivity.this);
+                    mDialog.setMessage("가입중입니다...");
+                    mDialog.show();
+
+                    //파이어베이스에 신규계정 등록하기
+                    firebaseAuth.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            //가입 성공시
+                            if (task.isSuccessful()) {
+                                mDialog.dismiss();
+
+                                FirebaseUser user = firebaseAuth.getCurrentUser();
+                                String email = user.getEmail();
+                                String uid = user.getUid();
+                                String name = mId.getText().toString().trim();
+
+                                //해쉬맵 테이블을 파이어베이스 데이터베이스에 저장
+                                HashMap<Object,String> hashMap = new HashMap<>();
+
+                                hashMap.put("uid",uid);
+                                hashMap.put("email",email);
+                                hashMap.put("id",name);
+
+                                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                                DatabaseReference reference = database.getReference("Users");
+                                reference.child(uid).setValue(hashMap);
+
+
+                                //가입이 이루어져을시 가입 화면을 빠져나감.
+                                Intent intent = new Intent(SignupActivity.this, MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                                Toast.makeText(SignupActivity.this, "회원가입에 성공하셨습니다.", Toast.LENGTH_SHORT).show();
+
+                            } else {
+                                mDialog.dismiss();
+                                Toast.makeText(SignupActivity.this, "이미 존재하는 아이디 입니다.", Toast.LENGTH_SHORT).show();
+                                return;  //해당 메소드 진행을 멈추고 빠져나감.
+
+                            }
+
+                        }
+                    });
+
+                    //비밀번호 오류시
+                }else{
+
+                    Toast.makeText(SignupActivity.this, "비밀번호가 틀렸습니다. 다시 입력해 주세요.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        });
 
     }
 
+    public boolean onSupportNavigateUp(){
+        onBackPressed();; // 뒤로가기 버튼이 눌렸을시
+        return super.onSupportNavigateUp(); // 뒤로가기 버튼
+    }
 }
