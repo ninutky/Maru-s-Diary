a77be995c39682cd839f00e49ba4e3a06d26f250
jihye1012 <s2127@e-mirim.hs.kr>
2023-06-13 12:52:16 +0900

    빠진 파일 커밋

---
+++ app/src/main/java/com/example/maru_s_diary/FirebaseID.java
@@ -0,0 +1,19 @@
+package com.example.maru_s_diary;
+
+public class FirebaseID {
+
+    public static String user="user";
+    public static String post="post";
+
+    public static String documentId="documentId";
+    public static String email="email";
+    public static String password="password";
+
+    public static String title="title";
+    public static String contents="contents";
+
+    public static int weather=1;
+    public static int feeling=1;
+    public static String date="2000/01/01";
+   // public static int heart=100;
+}
