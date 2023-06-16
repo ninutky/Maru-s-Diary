645ad1f3f6fcc90928ac66aa0d67635166928c02
Ye-jin Lee <s2108@e-mirim.hs.kr>
2023-06-16 11:41:50 +0900

    일기 수정, 일기 작성, 날씨 아이콘 변경, 날짜 EditText로 변경

--- app/src/main/java/com/example/maru_s_diary/Modification.java
+++ app/src/main/java/com/example/maru_s_diary/Modification.java
@@ -3,23 +3,32 @@ package com.example.maru_s_diary;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
-
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class Modification extends AppCompatActivity {
 
+    private TextView dialog;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+    Dialog dialog01; // 커스텀 다이얼로그
     Dialog dialog02; // 커스텀 다이얼로그
+    Dialog dialog03; // 커스텀 다이얼로그
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.writing_diary);
+
 
         dialog02 = new Dialog(Modification.this);       // Dialog 초기화
         dialog02.requestWindowFeature(Window.FEATURE_NO_TITLE); // 타이틀 제거
@@ -27,17 +36,84 @@ public class Modification extends AppCompatActivity {
 
         // 버튼: 커스텀 다이얼로그 띄우기
         findViewById(R.id.mood).setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
-                showDialog02(); // 아래 showDialog01() 함수 호출
+                showDialog02(); // 아래 showDialog02() 함수 호출
+
+
+            }
+        });
+
+        dialog03 = new Dialog(Modification.this);       // Dialog 초기화
+        dialog03.requestWindowFeature(Window.FEATURE_NO_TITLE); // 타이틀 제거
+        dialog03.setContentView(R.layout.dialog03);             // xml 레이아웃 파일과 연결
+
+        // 버튼: 커스텀 다이얼로그 띄우기
+        findViewById(R.id.weather).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog03(); // 아래 showDialog03() 함수 호출
+            }
+        });
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                String strDate = String.valueOf(year) + ".";
+                strDate += String.valueOf(month+1) + ".";
+                strDate += String.valueOf(day);
+
+                Toast.makeText(getApplicationContext(), strDate, Toast.LENGTH_SHORT).show();
+            }
+        };
+
+    }
+
+    // dialog01을 디자인하는 함수
+    public void showDialog01(){
+        dialog01.show(); // 다이얼로그 띄우기
+        dialog01.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT)); // 투명 배경
+        /* 이 함수 안에 원하는 디자인과 기능을 구현하면 된다. */
+
+        // 위젯 연결 방식은 각자 취향대로~
+        // '아래 아니오 버튼'처럼 일반적인 방법대로 연결하면 재사용에 용이하고,
+        // '아래 네 버튼'처럼 바로 연결하면 일회성으로 사용하기 편함.
+        // *주의할 점: findViewById()를 쓸 때는 -> 앞에 반드시 다이얼로그 이름을 붙여야 한다.
+
+        // 아니오 버튼
+        Button noBtn = dialog01.findViewById(R.id.noBtn);
+        noBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 원하는 기능 구현
+                dialog01.dismiss(); // 다이얼로그 닫기
+            }
+        });
+        // 네 버튼
+        dialog01.findViewById(R.id.yesBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 원하는 기능 구현
+                finish();           // 앱 종료
             }
         });
     }
 
-    // dialog01을 디자인하는 함수
     public void showDialog02(){
         dialog02.show(); // 다이얼로그 띄우기
         dialog02.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT)); // 투명 배경
+        /* 이 함수 안에 원하는 디자인과 기능을 구현하면 된다. */
+
+        // 위젯 연결 방식은 각자 취향대로~
+        // '아래 아니오 버튼'처럼 일반적인 방법대로 연결하면 재사용에 용이하고,
+        // '아래 네 버튼'처럼 바로 연결하면 일회성으로 사용하기 편함.
+        // *주의할 점: findViewById()를 쓸 때는 -> 앞에 반드시 다이얼로그 이름을 붙여야 한다.
+
+    }
+
+    public void showDialog03(){
+        dialog03.show(); // 다이얼로그 띄우기
+        dialog03.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT)); // 투명 배경
         /* 이 함수 안에 원하는 디자인과 기능을 구현하면 된다. */
 
         // 위젯 연결 방식은 각자 취향대로~
