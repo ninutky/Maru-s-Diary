099edf0cd9de8132b9416815742d1a82153547e1
yejin4117 <s2108@e-mirim.hs.kr>
2023-06-17 23:45:48 +0900

    일기작성 베이지 기분,날씨 다이얼로그 중간 커밋

--- app/src/main/java/com/example/maru_s_diary/Modification.java
+++ app/src/main/java/com/example/maru_s_diary/Modification.java
@@ -1,6 +1,5 @@
 package com.example.maru_s_diary;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
@@ -8,27 +7,67 @@ import android.app.Dialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import java.util.Calendar;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class Modification extends AppCompatActivity {
 
-    private TextView dialog;
-    private DatePickerDialog.OnDateSetListener mDateSetListener;
+    private static final String TAG = "Modification";
+
+//    private DatePickerDialog.OnDateSetListener mDateSetListener;
     Dialog dialog01; // 커스텀 다이얼로그
     Dialog dialog02; // 커스텀 다이얼로그
     Dialog dialog03; // 커스텀 다이얼로그
 
+    ImageView[] prfchks;
+    CircleImageView mood_img;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.writing_diary);
+        setContentView(R.layout.correction_diary);
+//        dialog = (TextView) findViewById(R.id.days);
+//        dialog = findViewById(R.id.date);
+//
+//        dialog.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Calendar cal = Calendar.getInstance();
+//                int year = cal.get(Calendar.YEAR);
+//                int month = cal.get(Calendar.MONTH);
+//                int day = cal.get(Calendar.DAY_OF_MONTH);
+//
+//                DatePickerDialog dialog = new DatePickerDialog(
+//                        Modification.this,
+//                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+//                        mDateSetListener,
+//                        year,month,day);
+//                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+//                dialog.show();
+//            }
+//        });
 
+        dialog01 = new Dialog(Modification.this);       // Dialog 초기화
+        dialog01.requestWindowFeature(Window.FEATURE_NO_TITLE); // 타이틀 제거
+        dialog01.setContentView(R.layout.dialog01);             // xml 레이아웃 파일과 연결
+
+        // 버튼: 커스텀 다이얼로그 띄우기
+        findViewById(R.id.delete).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog01(); // 아래 showDialog01() 함수 호출
+            }
+        });
 
         dialog02 = new Dialog(Modification.this);       // Dialog 초기화
         dialog02.requestWindowFeature(Window.FEATURE_NO_TITLE); // 타이틀 제거
@@ -39,10 +78,37 @@ public class Modification extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                showDialog02(); // 아래 showDialog02() 함수 호출
+                //Toast.makeText(getContext(), "기분이 저장되었습니다.",Toast.LENGTH_SHORT).show();
+                dialog02.dismiss();
 
+                // 프로필 저장
+                int selectedMoodIndex = -1;
+                for (int i = 0; i < 9; i++) {
+                    if (prfchks[i].getVisibility() == View.VISIBLE) {
+                        selectedMoodIndex = i;
+                        break;
+                    }
+                }
 
-
+                if (selectedMoodIndex == 0) {
+                    mood_img.setImageResource(R.drawable.mood1);
+                } else if (selectedMoodIndex == 1) {
+                    mood_img.setImageResource(R.drawable.mood2);
+                } else if (selectedMoodIndex == 2) {
+                    mood_img.setImageResource(R.drawable.mood3);
+                } else if (selectedMoodIndex == 3) {
+                    mood_img.setImageResource(R.drawable.mood4);
+                } else if (selectedMoodIndex == 4) {
+                    mood_img.setImageResource(R.drawable.mood5);
+                } else if (selectedMoodIndex == 5) {
+                    mood_img.setImageResource(R.drawable.mood6);
+                } else if (selectedMoodIndex == 6) {
+                    mood_img.setImageResource(R.drawable.mood7);
+                } else if (selectedMoodIndex == 7) {
+                    mood_img.setImageResource(R.drawable.mood8);
+                } else {
+                    mood_img.setImageResource(R.drawable.mood9);
+                }
             }
         });
 
@@ -57,17 +123,6 @@ public class Modification extends AppCompatActivity {
                 showDialog03(); // 아래 showDialog03() 함수 호출
             }
         });
-        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                String strDate = String.valueOf(year) + ".";
-                strDate += String.valueOf(month+1) + ".";
-                strDate += String.valueOf(day);
-
-                Toast.makeText(getApplicationContext(), strDate, Toast.LENGTH_SHORT).show();
-            }
-        };
-
     }
 
     // dialog01을 디자인하는 함수
--- app/src/main/java/com/example/maru_s_diary/SettingsFragment.java
+++ app/src/main/java/com/example/maru_s_diary/SettingsFragment.java
@@ -1,13 +1,10 @@
 package com.example.maru_s_diary;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
